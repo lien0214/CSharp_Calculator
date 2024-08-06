d6f5be5ba4269bbab07d02e7adb781878f94b67c
yiwei <yiwei_lian@cmoney.com.tw>
2024-08-06 09:46:42 +0800

    docs: CalculatorController comment

--- CalculatorAPI/Controllers/CalculatorController.cs
+++ CalculatorAPI/Controllers/CalculatorController.cs
@@ -5,10 +5,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CalculatorAPI.Controllers
 {
+    /// <summary>
+    /// The CalculatorController class is responsible for handling API requests to the calculator,
+    /// mapping button texts to their corresponding click actions, and managing the overall calculator logic.
+    /// </summary>
     [Route("[controller]")]
     [ApiController]
     public class CalculatorController : ControllerBase
     {
+        /// <summary>
+        /// Dictionary to map button text to the corresponding IClick implementation.
+        /// </summary>
         private readonly Dictionary<string, IClick> buttonText2Click = new()
         {
             { "1", new DigitDotClick() },
@@ -37,13 +44,26 @@ namespace CalculatorAPI.Controllers
             { "Backspace", new BackspaceClick() },
             { "±", new NegateClick() }
         };
+
         private readonly ILogger<CalculatorController> _logger;
         private readonly CoreControl CoreController;
-        public CalculatorController(ILogger<CalculatorController> logger, CoreControl coreController, UIContext formContext)
+
+        /// <summary>
+        /// Initializes a new instance of the CalculatorController class.
+        /// </summary>
+        /// <param name="logger">Logger instance for logging.</param>
+        /// <param name="coreController">Core controller for calculator logic.</param>
+        public CalculatorController(ILogger<CalculatorController> logger, CoreControl coreController)
         {
             _logger = logger;
             CoreController = coreController;
         }
+
+        /// <summary>
+        /// Handles POST requests to perform calculator actions based on button text input.
+        /// </summary>
+        /// <param name="buttonText">The text of the calculator button pressed.</param>
+        /// <returns>An IActionResult that is either Ok with the result or BadRequest on invalid input.</returns>
         [HttpPost(Name = "PostCalculator")]
         public IActionResult Post([FromBody] string buttonText)
         {
