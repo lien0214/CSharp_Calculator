80c8547f26835ccbbb2a136027a1a0ddba5c52c7
yiwei <yiwei_lian@cmoney.com.tw>
2024-08-06 08:59:41 +0800

    feat: merge coreController partial class

--- CalculatorCore/CoreController.cs
+++ CalculatorCore/CoreController.cs
@@ -2,7 +2,7 @@
 using CalculatorCore.Utils;
 namespace CalculatorCore
 {
-    public partial class CoreController
+    public class CoreController
     {
         internal CoreContext Context { get; private set; }
         internal IState State { get => Context.State; private set => Context.State = value; }
@@ -18,6 +18,60 @@ namespace CalculatorCore
         {
             bool isNotError = State != Maps.StateMap[StateType.ErrorState];
             return new(Context.Input, Context.Output, Context.Prefix(), Context.Infix(), Context.Postfix(), isNotError);
+        }
+
+        internal UIContext EnterDigitDot(char digitDot)
+        {
+            State.EnterDigitDot(this, digitDot);
+            return PackContext();
+        }
+        internal UIContext EnterOperator(char op)
+        {
+            State.EnterOperator(this, op);
+            return PackContext();
+        }
+        internal UIContext EnterSquareRoot()
+        {
+            State.EnterSquareRoot(this);
+            return PackContext();
+        }
+        internal UIContext EnterEqual()
+        {
+            State.EnterEqual(this);
+            return PackContext();
+        }
+
+        internal UIContext EnterBackspace()
+        {
+            State.EnterBackspace(this);
+            return PackContext();
+        }
+        internal UIContext EnterClear()
+        {
+            State.EnterClear(this);
+            return PackContext();
+        }
+        internal UIContext EnterClearEntry()
+        {
+            State.EnterClearEntry(this);
+            return PackContext();
+        }
+        internal UIContext EnterNegate()
+        {
+            State.EnterNegate(this);
+            return PackContext();
+        }
+
+        internal UIContext EnterLeftParenthesis()
+        {
+            State.EnterLeftParenthesis(this);
+            return PackContext();
+        }
+
+        internal UIContext EnterRightParenthesis()
+        {
+            State.EnterRightParenthesis(this);
+            return PackContext();
         }
     }
 }
